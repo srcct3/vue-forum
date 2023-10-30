a9e816593a84b1b7d0905ceb9b9cdbb4d0b225f4
srcct3 <src.ct3@gmail.com>
2023-10-30 19:22:09 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import PageHome from "./components/PageHome.vue";
+
+export default {
+  components: {
+    PageHome,
+  },
+};
+</script>
+
+<template>
+  <p>App</p>
+  <page-home />
+</template>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,30 @@
+<script>
+import sourceData from "../data.json";
+
+export default {
+    data: () => ({
+        threads: sourceData.threads,
+        posts: sourceData.posts,
+        users: sourceData.users,
+    }),
+    methods: {
+        postById(postId) {
+            return this.posts.find(p => p.id === postId)
+        },
+        userById(userId) {
+            return this.users.find(u => u.id === userId)
+        }
+    }
+};
+</script>
+
+<template>
+    <h1>Threads</h1>
+    <div v-for="thread in threads" :key="thread.id">
+        <h2>{{ thread.title }}</h2>
+        <div v-for="postId in thread.posts" :key="postId">
+            <p>{{ userById(postById(postId).userId).name }}</p>
+            <p>{{ postById(postId).text }}</p>
+        </div>
+    </div>
+</template>
